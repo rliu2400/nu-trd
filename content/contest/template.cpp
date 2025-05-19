7d8ef7fb4d0c4caed469cd1ebf5239bfc7587025
Ruyi Lan <rliu2400@gmail.com>
2025-05-19 13:18:55 -0500

    new template

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,14 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define bg(x) begin(x)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
+void solve() {
+
+}
+
 int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	cin.exceptions(cin.failbit);
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    void solve();
+
+    return 0;
 }
