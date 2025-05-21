b7e3e94e651332efeeba75490b640059102fc6ea
Ruyi Lan <rliu2400@gmail.com>
2025-05-21 15:33:34 -0500

    lazy seg tree

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -1,66 +1,72 @@
-/**
- * Author: Simon Lindholm
- * Date: 2016-10-08
- * License: CC0
- * Source: me
- * Description: Segment tree with ability to add or set values of large intervals, and compute max of intervals.
- * Can be changed to other things.
- * Use with a bump allocator for better performance, and SmallPtr or implicit indices to save memory.
- * Time: O(\log N).
- * Usage: Node* tr = new Node(v, 0, sz(v));
- * Status: stress-tested a bit
- */
-#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+class SegmentTree { // OOP style
+  private:
+    int n;                                // n = (int)A.size()
+    vi A, st, lazy;                       // the arrays
+    int l(int p) { return p << 1; }       // go to left child
+    int r(int p) { return (p << 1) + 1; } // go to right child
+    int conquer(int a, int b) {
+        if (a == -1)
+            return b; // corner case
+        if (b == -1)
+            return a;
+        return min(a, b); // RMQ
+    }
+    void build(int p, int L, int R) { // O(n)
+        if (L == R)
+            st[p] = A[L]; // base case
+        else {
+            int m = (L + R) / 2;
+            build(l(p), L, m);
+            build(r(p), m + 1, R);
+            st[p] = conquer(st[l(p)], st[r(p)]);
+        }
+    }
+    void propagate(int p, int L, int R) {
+        if (lazy[p] != -1) {                       // has a lazy flag
+            st[p] = lazy[p];                       // [L..R] has same value
+            if (L != R)                            // not a leaf
+                lazy[l(p)] = lazy[r(p)] = lazy[p]; // propagate downwards
+            else                                   // L == R, a single index
+                A[L] = lazy[p];                    // time to update this
+            lazy[p] = -1;                          // erase lazy flag
+        }
+    }
+    int RMQ(int p, int L, int R, int i, int j) { // O(log n)
+        propagate(p, L, R);                      // lazy propagation
+        if (i > j)
+            return -1; // infeasible
+        if ((L >= i) && (R <= j))
+            return st[p]; // found the segment
+        int m = (L + R) / 2;
+        return conquer(RMQ(l(p), L, m, i, min(m, j)), RMQ(r(p), m + 1, R, max(i, m + 1), j));
+    }
 
-#include "../various/BumpAllocator.h"
+    void update(int p, int L, int R, int i, int j, int val) { // O(log n)
+        propagate(p, L, R);                                   // lazy propagation
+        if (i > j)
+            return;
+        if ((L >= i) && (R <= j)) { // found the segment
+            lazy[p] = val;          // update this
+            propagate(p, L, R);     // lazy propagation
+        } else {
+            int m = (L + R) / 2;
+            update(l(p), L, m, i, min(m, j), val);
+            update(r(p), m + 1, R, max(i, m + 1), j, val);
+            int lsubtree = (lazy[l(p)] != -1) ? lazy[l(p)] : st[l(p)];
+            int rsubtree = (lazy[r(p)] != -1) ? lazy[r(p)] : st[r(p)];
+            st[p] = (lsubtree <= rsubtree) ? st[l(p)] : st[r(p)];
+        }
+    }
 
-const int inf = 1e9;
-struct Node {
-	Node *l = 0, *r = 0;
-	int lo, hi, mset = inf, madd = 0, val = -inf;
-	Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of -inf
-	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
-		if (lo + 1 < hi) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
-			val = max(l->val, r->val);
-		}
-		else val = v[lo];
-	}
-	int query(int L, int R) {
-		if (R <= lo || hi <= L) return -inf;
-		if (L <= lo && hi <= R) return val;
-		push();
-		return max(l->query(L, R), r->query(L, R));
-	}
-	void set(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) mset = val = x, madd = 0;
-		else {
-			push(), l->set(L, R, x), r->set(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void add(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) {
-			if (mset != inf) mset += x;
-			else madd += x;
-			val += x;
-		}
-		else {
-			push(), l->add(L, R, x), r->add(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void push() {
-		if (!l) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(lo, mid); r = new Node(mid, hi);
-		}
-		if (mset != inf)
-			l->set(lo,hi,mset), r->set(lo,hi,mset), mset = inf;
-		else if (madd)
-			l->add(lo,hi,madd), r->add(lo,hi,madd), madd = 0;
-	}
+  public:
+    SegmentTree(int sz) : n(sz), st(4 * n), lazy(4 * n, -1) {}
+    SegmentTree(const vi &initialA) : SegmentTree((int)initialA.size()) {
+        A = initialA;
+        build(1, 0, n - 1);
+    }
+    void update(int i, int j, int val) { update(1, 0, n - 1, i, j, val); }
+    int RMQ(int i, int j) { return RMQ(1, 0, n - 1, i, j); }
 };
